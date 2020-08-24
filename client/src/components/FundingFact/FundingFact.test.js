99649b0d71ff758a945bf17aa4b8ec744be8efb5
“Atessadai” <Atessa.dailami@gmail.com>
2020-08-24 14:26:11 -0400

    edited icon

--- client/src/components/FundingFact/FundingFact.test.js
+++ client/src/components/FundingFact/FundingFact.test.js
@@ -1,25 +1,14 @@
 import React from 'react'
 import { shallow, mount, configure } from 'enzyme'
-<<<<<<< HEAD:client/src/components/IconLogin/IconLogin.test.js
-import IconLogin from './IconLogin.js'
-=======
 import FundingFact from './FundingFact.js'
->>>>>>> ed745d7500ec38357d97d54ab25efbfed001eec6:client/src/components/FundingFact/FundingFact.test.js
 import Adapter from 'enzyme-adapter-react-16';
-configure({adapter: new Adapter()});
+configure({ adapter: new Adapter() });
 
 
-<<<<<<< HEAD:client/src/components/IconLogin/IconLogin.test.js
-describe('IconLogin component', () => {
-
-  it('should render as expected', () => {
-    const component = shallow(<IconLogin />)
-=======
 describe('FundingFact component', () => {
 
   it('should render as expected', () => {
     const component = shallow(<FundingFact />)
->>>>>>> ed745d7500ec38357d97d54ab25efbfed001eec6:client/src/components/FundingFact/FundingFact.test.js
-	 	expect(component.exists()).toBe(true);
+    expect(component.exists()).toBe(true);
   })
 })

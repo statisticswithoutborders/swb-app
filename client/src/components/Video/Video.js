3b103908820196f29c781e37a445fd9746775c85
Alex_Kelly <afkelly14@Gmail.com>
2020-08-24 14:54:13 -0400

    added the Faunalytics video

--- client/src/components/Video/Video.js
+++ client/src/components/Video/Video.js
@@ -2,11 +2,12 @@ import React from 'react';
 import "./Video.css";
 import ReactPlayer from "react-player"
 
+//faunalytics video
 function Video() {
   return (
     <div>
-      <ReactPlayer
-        url="https://swb.wildapricot.org/resources/Videos/swb_2_org_structure.mp4"
+      <ReactPlayer className='video'
+        url="https://www.youtube.com/watch?v=xFKkTMjQoIw"
       />
     </div>
   )

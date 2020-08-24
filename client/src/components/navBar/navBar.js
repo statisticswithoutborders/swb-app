800e1bbe9c3f3d6abbe7acecce109a3364b909f9
Michael Hall <mhalla40@gmail.com>
2020-08-24 17:29:46 -0400

    added css

--- client/src/components/navBar/navBar.js
+++ client/src/components/navBar/navBar.js
@@ -1,41 +1,39 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import "./navBar.css";
+import "./NavBar.css";
 
 class NavBar extends Component {
   render() {
     return (
       <nav className="Nav">
         <div>
-          <hr></hr>
-          <ul className="Nav__item-wrapper">
-            <li className="Nav__item">
-              <Link className="Nav__link" to="#">
+          <ul className="Nav_item-wrapper">
+            <li className="Nav_item">
+              <Link className="Nav_link" to="#">
                 Services
               </Link>
             </li>
-            <li className="Nav__item">
-              <Link className="Nav__link" to="#">
+            <li className="Nav_item">
+              <Link className="Nav_link" to="#">
                 Projects
               </Link>
             </li>
-            <li className="Nav__item">
-              <Link className="Nav__link" to="#">
+            <li className="Nav_item">
+              <Link className="Nav_link" to="#">
                 Volunteers
               </Link>
             </li>
-            <li className="Nav__item">
-              <Link className="Nav__link" to="#">
+            <li className="Nav_item">
+              <Link className="Nav_link" to="#">
                 About
               </Link>
             </li>
-            <li className="Nav__item">
-              <Link className="Nav__link" to="#">
+            <li className="Nav_item">
+              <Link className="Nav_link" to="#">
                 Contact
               </Link>
             </li>
           </ul>
-          <hr></hr>
         </div>
       </nav>
     );
--- client/src/components/navBar/navBar.stories.js
+++ client/src/components/navBar/navBar.stories.js
@@ -1,41 +1,10 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import "./navBar.css";
+import StoryRouter from "storybook-react-router";
 
-import navBar from "./navBar";
+import "./NavBar.css";
+import NavBar from "./NavBar";
 
-storiesOf("navBar", module).add("navBar", () => {
-  return (
-    <div>
-      <hr></hr>
-      <ul className="Nav__item-wrapper">
-        <li className="Nav__item">
-          <a className="Nav__link" href="#">
-            Services
-          </a>
-        </li>
-        <li className="Nav__item">
-          <a className="Nav__link" href="#">
-            Projects
-          </a>
-        </li>
-        <li className="Nav__item">
-          <a className="Nav__link" href="#">
-            Volunteers
-          </a>
-        </li>
-        <li className="Nav__item">
-          <a className="Nav__link" href="#">
-            About
-          </a>
-        </li>
-        <li className="Nav__item">
-          <a className="Nav__link" href="#">
-            Contact
-          </a>
-        </li>
-      </ul>
-      <hr></hr>
-    </div>
-  );
-});
+storiesOf("NavBar", module)
+  .addDecorator(StoryRouter())
+  .add("NavBar", () => <NavBar />);

cef868b067024dddc3db2e645932b7aab11d17f9
butterfly1of4 <jhouck29@gmail.com>
2020-08-24 15:21:16 -0400

    rendering

--- client/src/components/IconNumber/IconNumber.js
+++ client/src/components/IconNumber/IconNumber.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import "./IconNumber.css";
-// import "../../index.css"
+import "./IconNumber.stories"
+import "../../index.css"
 
 // Function based React Component
 const IconNumber = (props) => {
 
   // Default Class to apply to Component
   // let classList = "IconNumber"
-  let classList = ""
-
+  let classList = "icon-number"
+  
   let numbers = ["digit"]
+  console.log(numbers, classList, props)
   if (numbers.includes(props.number)){
+    console.log(props.number)
     classList += `0${props.number}`
-    console.log(classList)
   }
+  console.log(classList)
   
   return(
+    <div className={"box"}> 
     <div className={classList}>
       {props.label}
+    <div></div>
+
+      </div>
     </div>
-  );
+  )
 }
 export default IconNumber
--- client/src/components/IconNumber/IconNumber.stories.js
+++ client/src/components/IconNumber/IconNumber.stories.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-// import "../../index.css"
+import "../../index.css"
 import IconNumber from "./IconNumber.js" 
 import './IconNumber.css';
 
 
-
+// let numberLabel = props.label
 
 storiesOf('IconNumber', module)
     .add('IconNumber', () =>{
-      return <IconNumber box label="01"/>
+      return <IconNumber type="digit" IconNumber box label="01"/>
     }
 )

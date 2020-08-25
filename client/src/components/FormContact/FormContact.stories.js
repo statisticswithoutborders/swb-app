9e7150177490fab4822d7b70d9d8adce5a0104e2
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-08-25 14:25:27 -0400

    Add help and volunteer stories

--- client/src/components/FormContact/FormContact.stories.js
+++ client/src/components/FormContact/FormContact.stories.js
@@ -2,6 +2,13 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import FormContact from "./FormContact";
 
-storiesOf("FormContact", module).add("FormContact", () => {
-	return <FormContact formCallback={this.handleSend} />;
-});
+storiesOf("FormContact", module)
+	.add("FormContact", () => {
+		return <FormContact formCallback={this.handleSend} />;
+	})
+	.add("FormContactHelp", () => {
+		return <FormContact help formCallback={this.handleSend} />;
+	})
+	.add("FormContactVolunteer", () => {
+		return <FormContact volunteer formCallback={this.handleSend} />;
+	});

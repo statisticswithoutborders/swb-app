4c652bc017d7084a1da135a40ee0bfd1902b6515
Michael Hall <mhalla40@gmail.com>
2020-08-25 16:43:07 -0400

    created header

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
+import nav_logo_world from "../../assets/images/nav_logo_world.png";
+import nav_logo_name from "../../assets/images/nav_logo_name.png";
+import nav_user_login from "../../assets/images/nav_user_login.png";
 
 class NavBar extends Component {
   render() {
     return (
       <nav className="Nav">
-        <div>
+        <div className="Header">
+          <img src={nav_logo_world} className="logo" alt="world" />
+
+          <img src={nav_logo_name} className="logo" alt="name" />
           <ul className="Nav_item-wrapper">
             <li className="Nav_item">
               <Link className="Nav_link" to="#">
@@ -33,6 +39,9 @@ class NavBar extends Component {
                 Contact
               </Link>
             </li>
+            <a href="#">
+              <img src={nav_user_login} alt="login" />
+            </a>
           </ul>
         </div>
       </nav>

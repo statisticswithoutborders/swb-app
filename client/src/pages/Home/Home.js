b6a384421d9b1872565641227e312240f176945f
Jeremiah <jscott4720@gmail.com>
2020-08-25 18:08:44 -0400

    added styling to banner*

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import "./Home.css";
-import homeBanner from '../../assets/images/home_banner.png';
+import homeBanner from "../../assets/images/home_banner.png";
 import NavBar from "../../components/NavBar/NavBar";
-import Button from '../../components/Button/Button'
+import Button from "../../components/Button/Button";
 
 class Home extends Component {
   constructor() {
@@ -14,10 +14,17 @@ class Home extends Component {
     return (
       <React.Fragment>
         <NavBar />
-        <div className = 'banner'>
+        <div className="banner">
+          <div className= 'words'>
             <h1>We Use Data For Good</h1>
-            <p>Statistics Without Borders is a non-profit volunteer outreach group of the American Statistical Association. We provide pro-bono consultancy and training in statistics and data science to promote objective and transparent decision making.</p>
-            <Button large label= 'LEARN MORE'/>
+            <p className="statement">
+              Statistics Without Borders is a non-profit volunteer outreach
+              group of the American Statistical Association. We provide pro-bono
+              consultancy and training in statistics and data science to promote
+              objective and transparent decision making.
+            </p>
+            <Button large label="LEARN MORE" />
+          </div>
         </div>
       </React.Fragment>
     );

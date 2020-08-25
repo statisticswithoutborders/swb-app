7f3be45e7963ac7716b04b5310beac7d1f0e1c56
Alex_Kelly <afkelly14@Gmail.com>
2020-08-25 13:46:22 -0400

    fixed navbar issue

---
+++ client/src/components/navBar/NavBar.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className="Nav">
+        <div>
+          <ul className="Nav_item-wrapper">
+            <li className="Nav_item">
+              <Link className="Nav_link" to="#">
+                Services
+              </Link>
+            </li>
+            <li className="Nav_item">
+              <Link className="Nav_link" to="#">
+                Projects
+              </Link>
+            </li>
+            <li className="Nav_item">
+              <Link className="Nav_link" to="#">
+                Volunteers
+              </Link>
+            </li>
+            <li className="Nav_item">
+              <Link className="Nav_link" to="#">
+                About
+              </Link>
+            </li>
+            <li className="Nav_item">
+              <Link className="Nav_link" to="#">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;

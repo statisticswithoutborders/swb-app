95dae8036557059238ed247421519cf14c88826c
mike_visser <mpviss@gmail.com>
2020-08-25 11:39:40 -0400

    created projectcard component

---
+++ client/src/components/ProjectCard/ProjectCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./ProjectCard.css";
+
+// Function based React Component
+const ProjectCard = (props) => {
+
+  // Default Class to apply to Component
+  let classList = `ProjectCard`;
+
+
+  return(
+    <div className={classList}>
+      ProjectCard
+    </div>
+  );
+}
+
+export default ProjectCard;

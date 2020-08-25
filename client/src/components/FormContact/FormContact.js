2adc5da32d3e1c4bc35d80369fdd08377a064a50
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-08-25 14:36:38 -0400

    Add state properties and handler methods

--- client/src/components/FormContact/FormContact.js
+++ client/src/components/FormContact/FormContact.js
@@ -1,22 +1,42 @@
-import React, { Component } from 'react'
-import "./FormContact.css"
+import React, { Component } from "react";
+import "./FormContact.css";
+import Button from "../Button/Button";
+import Input from "../Input/Input";
 
-// React Class Component
+// Controlled Component
 class FormContact extends Component {
-    constructor(){
-        super()
+	constructor() {
+		super();
+		this.state = {
+			classlist: "FormContact",
+			name: "",
+			email: "",
+			organization: "",
+			subject: "",
+			message: "",
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	} //constructor()
 
-         this.state ={classlist:'FormContact'}
-    }
+	handleChange(e) {
+		this.setState({ [e.target.name]: e.target.value });
+	} //handleChange()
 
+	handleSubmit(e) {
+		e.preventDefault();
+		this.props.formCallback(
+			this.state.name,
+			this.state.email,
+			this.state.organization,
+			this.state.subject,
+			this.state.message
+		);
+	}
 
-    render() {
-        return (
-            <div className={classList}>
-
-            </div>
-        )
-    }
+	render() {
+		return <div className={classList}></div>;
+	}
 }
 
-export default FormContact
+export default FormContact;

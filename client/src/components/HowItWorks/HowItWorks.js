d91f12479341bf5018dbb61b8b294f99bcf76ec7
mike_visser <mpviss@gmail.com>
2020-08-25 16:05:28 -0400

    added step and path images

--- client/src/components/HowItWorks/HowItWorks.js
+++ client/src/components/HowItWorks/HowItWorks.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import "./HowItWorks.css";
 import volunteersReview from '../../assets/images/volunteers_review.png'
+import volunteersEmailReceive from '../../assets/images/volunteers_email_receive.png'
+import volunteersEmailRespond from '../../assets/images/volunteers_email_respond.png'
+import volunteersCrown from '../../assets/images/volunteers_crown.png'
+import volunteersPath from '../../assets/images/volunteers_path.png'
 
 // Function based React Component
 const HowItWorks = (props) => {
@@ -15,10 +19,12 @@ const HowItWorks = (props) => {
       <div className="HowItWorks-body-container">
         <div className="HowItWorks-step-image-container">
           <img className="HowItWorks-step-image" src={volunteersReview} />
-          <img className="HowItWorks-step-image" src={volunteersReview} />
-          <img className="HowItWorks-step-image" src={volunteersReview} />
-          <img className="HowItWorks-step-image" src={volunteersReview} />
-          
+          <img className="HowItWorks-step-image" src={volunteersEmailRespond} />
+          <img className="HowItWorks-step-image" src={volunteersEmailReceive} />
+          <img className="HowItWorks-step-image" src={volunteersCrown} />
+        </div>
+        <div className="HowItWorks-path-container" >
+          <img className="HowItWorks-path-image" src={volunteersPath} />
         </div>
       </div>
     </div>

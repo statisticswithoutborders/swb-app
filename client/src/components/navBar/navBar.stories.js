e48e7c83f8de8a2c60f93301a14e7ff858c12a5a
Noah Clark <noahbc08@gmail.com>
2020-08-25 12:02:54 -0400

    tweaked CSS names and file names to capital camel case

--- client/src/components/navBar/navBar.stories.js
+++ client/src/components/navBar/navBar.stories.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import StoryRouter from "storybook-react-router";
-
 import "./NavBar.css";
-import NavBar from "./NavBar";
+import NavBar from "./NavBar.js";
+
 
 storiesOf("NavBar", module)
   .addDecorator(StoryRouter())
--- client/src/components/navBar/navBar.test.js
+++ client/src/components/navBar/navBar.test.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { shallow, mount, configure } from "enzyme";
-import Input from "./Input.js";
+import NavBar from "./NavBar.js";
 import Adapter from "enzyme-adapter-react-16";
 configure({ adapter: new Adapter() });
 
 describe("Input component", () => {
   it("should render as expected", () => {
-    const component = shallow(<Input />);
+    const component = shallow(<NavBar />);
     expect(component.exists()).toBe(true);
   });
 });

049302d1c9653d33fc02321dea1e5d9b6ceda2e6
mike_visser <mpviss@gmail.com>
2020-08-25 12:20:56 -0400

    added link to button

--- client/src/components/ProjectCard/ProjectCard.js
+++ client/src/components/ProjectCard/ProjectCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./ProjectCard.css";
 import Button from '../Button/Button'
+import { Link } from 'react-router-dom'
 
 // Function based React Component
 const ProjectCard = (props) => {
@@ -14,7 +15,9 @@ const ProjectCard = (props) => {
       <h3>{props.projectTitle}</h3>
       <div className="ProjectCard-image" style={{backgroundImage: props.image}}></div>
       {props.projectDescription ? <p className="ProjectCard-description">{props.projectDescription}</p> :null}
+      <Link to ="">
       <Button type = 'primary' medium  callback label="LEARN MORE" />
+      </Link>
     </div>
   );
 }

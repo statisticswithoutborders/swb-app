ecb5ed1a3237a6ee2837925b1ed289d07974e779
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-08-25 12:20:39 -0400

    Add FormContact initial files

---
+++ client/src/components/FormContact/FormContact.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import "./FormContact.css"
+
+// React Class Component
+class FormContact extends Component {
+    constructor(){
+        super()
+
+         this.state ={classlist:'FormContact'}
+    }
+
+
+    render() {
+        return (
+            <div className={classList}>
+
+            </div>
+        )
+    }
+}
+
+export default FormContact
---
+++ client/src/components/FormContact/FormContact.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import FormContact from './FormContact';
+
+
+storiesOf('FormContact', module)
+    .add('FormContact', () =>{
+      return <FormContact />
+    }
+)

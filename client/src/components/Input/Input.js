1f81d629539af40057b3f3ceb7fe5cee8a3b5be9
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-08-25 16:26:45 -0400

    Refactor Input to provide props needed for Form

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -2,24 +2,29 @@ import React from "react";
 import "../../index.css";
 
 // React Functional Component
-const Input = props => {
-  let classList = "";
+const Input = (props) => {
+	const { name, type, placeholder, value, onChange } = props;
+	let classList = "Input";
 
-  if (props.small) {
-    classList += ` Input-small`;
-  }
-  if (props.large) {
-    classList += ` Input-large`;
-  }
+	// if (props.small) {
+	// 	classList += ` Input-small`;
+	// }
+	if (props.large) {
+		classList += ` Input-large`;
+	}
 
-  return (
-    <form>
-      <textarea
-        placeholder={props.placeholder}
-        className={classList}
-      ></textarea>
-    </form>
-  );
+	return (
+		<input
+			name={name}
+			type={type}
+			placeholder={placeholder}
+			value={value}
+			onChange={onChange}
+
+			// placeholder={props.placeholder}
+			// className={classList}
+		></input>
+	);
 };
 
 export default Input;
--- client/src/components/Input/Input.stories.js
+++ client/src/components/Input/Input.stories.js
@@ -6,15 +6,23 @@ import "../../index.css";
 
 // Form inputs
 storiesOf("Input", module)
-  .add("Name", () => <Input small placeholder="NAME" type="regular" />)
-  .add("Email", () => <Input small placeholder="E-MAIL" type="regular" />)
-  .add("Organization", () => (
-    <Input small placeholder="ORGANIZATION" type="regular" />
-  ))
-  .add("Subject", () => <Input small placeholder="SUBJECT" type="regular" />)
-  .add("Message", () => (
-    <Input large placeholder="MESSAGE" type="input-large" />
-  ))
-  .add("Leave a Comment", () => (
-    <Input large placeholder="LEAVE A COMMENT" type="input-large" />
-  ));
+	.add("Name", () => (
+		<Input
+			name="name"
+			type="text"
+			placeholder=" NAME"
+			value={this.state.name}
+			onChange={this.handleChange}
+		/>
+	))
+	.add("Email", () => <Input small placeholder="EMAIL" type="regular" />)
+	.add("Organization", () => (
+		<Input small placeholder="ORGANIZATION" type="regular" />
+	))
+	.add("Subject", () => <Input small placeholder="SUBJECT" type="regular" />)
+	.add("Message", () => (
+		<Input large placeholder="MESSAGE" type="input-large" />
+	))
+	.add("Leave a Comment", () => (
+		<Input large placeholder="LEAVE A COMMENT" type="input-large" />
+	));

37a597c4d77fe2f5372f882237ebe7b6f2572091
mike_visser <mpviss@gmail.com>
2020-08-25 12:02:48 -0400

    added class and css fordescription

--- client/src/components/ProjectCard/ProjectCard.js
+++ client/src/components/ProjectCard/ProjectCard.js
@@ -11,7 +11,7 @@ const ProjectCard = (props) => {
 
   return(
     <div className={classList}>
-      <h4>{props.projectTitle}</h4>
+      <h3>{props.projectTitle}</h3>
       <div className="ProjectCard-image" style={{backgroundImage: props.image}}></div>
       <p className="ProjectCard-description">{props.projectDescription}</p>
       <Button type = 'primary' medium  callback label="LEARN MORE" />

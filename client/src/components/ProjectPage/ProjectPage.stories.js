b49b870d13edb96206edb3820d6e239a3c315c4d
AnthonyMaddox <anthonyjmaddox@gmail.com>
2020-08-25 15:46:33 -0400

    lost figma access somehow

--- client/src/components/ProjectPage/ProjectPage.stories.js
+++ client/src/components/ProjectPage/ProjectPage.stories.js
@@ -1,27 +1,12 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import "../../index.css";
-import ProjectPage from "./ProjectPage";
-import ServiceTag from "../ServiceTag/ServiceTag";
 import ProjectPageAsante from "./ProjectPageAsante";
 import ProjectPageEthiopia from "./ProjectPageEthiopia";
 import ProjectPageChimp from "./ProjectPageChimp";
+import ProjectPageSyria from "./ProjectPageSyria";
 
 storiesOf("ProjectPage", module)
-  .add("ProjectPageOne", () => {
-    return (
-      <ProjectPage
-        projectPageTitle="Save the Children - Ethiopia"
-        projectPageImage={"var(--project1-asante)"}
-        projectPageLocation="Kenya & Tanzania"
-        projectPageServicesOne={<ServiceTag label="data analysis" />}
-        projectPageServicesTwo={<ServiceTag label="monitoring & evaluation" />}
-        projectPageServicesThree=""
-        projectPageServicesFour=""
-        projectPageDescription="The Asante Africa Foundation works to empower East African youth with programs in Girls' Advancement, Accelerated Classroom Learning, and Youth Leadership and Entrepeneurship. Monitoring and Evaluation (M&E) is essential to showing the impact of their work and planning future efforts. In 2018, three SWB volunteers (two from Africa and one from India) delivered six 2-hour webinars on M&E to the East African staff. The webinars were a combination of lectures introducing basic concepts and terminology, and discussions about how to improve on-going projects. One of the SWB volunteers now has a connection to the Asante team and will continue to serve as a mentor."
-      />
-    );
-  })
   .add("ProjectPageAsante", () => {
     return <ProjectPageAsante />;
   })
@@ -30,4 +15,7 @@ storiesOf("ProjectPage", module)
   })
   .add("ProjectPageChimp", () => {
     return <ProjectPageChimp />;
+  })
+  .add("ProjectPageSyria", () => {
+    return <ProjectPageSyria />;
   });
---
+++ client/src/components/ProjectPage/ProjectPageSyria.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./ProjectPage.css";
+import ProjectImageGallery from "../ProjectImageGallery/ProjectImageGallery";
+import ServiceTag from "../ServiceTag/ServiceTag";
+
+// Function based React Component
+const ProjectPageSyria = (props) => {
+  // Default Class to apply to Component
+  let classList = `ProjectPage`;
+
+  return (
+    <div className="asantePageDiv">
+      <div className="projectPageTitle">Whole of Syria Health Cluster</div>
+      <div className="smallProjectTag">-project4-un</div>
+      <div>
+        <div className="pPageImageGalleryDiv">
+          <ProjectImageGallery />
+        </div>
+        <div>
+          <div>Location: Syria</div>
+          <div>
+            <div className="pPageServices">
+              <div>Services Provided:</div> <ServiceTag label="data analysis" />
+              <ServiceTag label="CART models" />
+            </div>
+            <div></div>
+          </div>
+          Description: The Syrian Health Cluster, with four hubs and 118
+          partners, is the coordination mechanism to provide emergency and
+          trauma care, as well as to provide basic health provision where it has
+          collapsed. The Syrian Health Cluster is a part of the "Whole-of-Syria"
+          approach, which allows for a more effective and accountable
+          humanitarian action inside Syria. By request from Luis Hernando
+          Aguilar of the Cluster at the time, a team of six SWB volunteers -
+          from England, France, Kenya, the Netherlands, and the United States -
+          spent one month completing and documenting an automatic data
+          compilation system using ETL scripts in R to replace Excel macros. The
+          scripts helped optimize the ETL processes, reducing effort time,
+          minimizing error risks, and improving the quality of the data that
+          were the basis for decision making.
+        </div>
+        <br></br>
+        <br></br>
+      </div>
+      <div>
+        Humanitarian Response website<br></br>
+        <br></br>UN Office for the Coordination of Humanitarian Affairs
+      </div>
+    </div>
+  );
+};
+
+export default ProjectPageSyria;

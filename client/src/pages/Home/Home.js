7e10fb81fb14f51af1374f6b523b8f65e627f3f1
Jeremiah <jscott4720@gmail.com>
2020-08-26 11:42:23 -0400

    finished styling on 'Our Mission'

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -4,6 +4,7 @@ import NavBar from "../../components/NavBar/NavBar";
 import Button from "../../components/Button/Button";
 import Globe from "../../assets/images/home_globe.png";
 import Analysis from "../../assets/images/home_analysis.png";
+import Bullet from "../../assets/images/Slice 1.png";
 
 class Home extends Component {
   constructor() {
@@ -38,31 +39,55 @@ class Home extends Component {
           through the proper application of statistical principles and best
           practices, where access to such resources is limited
         </h3>
-        <ul>
-          <li>
+      
+
+        <div className="bulletpoint">
+          {" "}
+          <img src={Bullet} className="bullet" />
+          <div className="point">
+            {" "}
             Advocate objective, impartial, and sound decision making using best
             statistical practices
-          </li>
-
-          <li>
+          </div>
+        </div>
+        <div className="bulletpoint">
+          {" "}
+          <img src={Bullet} className="bullet" />
+          <div className="point">
+            {" "}
             Provide analytical services through client projects for public
             benefit that support goals that are nonreligious, non-partisan, and
             non-personal, with a focus on developing countries
-          </li>
-          <li>
+          </div>
+        </div>
+        <div className="bulletpoint">
+          {" "}
+          <img src={Bullet} className="bullet" />
+          <div className="point">
+            {" "}
             Assist organizations outside of the for-profit sector, with
             priorities given to organizations with limited access to statistical
             resources
-          </li>
-          <li>
+          </div>
+        </div>
+        <div className="bulletpoint">
+          {" "}
+          <img src={Bullet} className="bullet" />
+          <div className="point">
+            {" "}
             Strengthen its clients and their communities by helping them build
             their capacities in statistics, data science and analytics, and
             promotes best practices in these areas
-          </li>
-        </ul>
-        <hr/>
+          </div>
+        </div>
+        <hr />
         <h2>What We Do</h2>
-        <p>We work to improve decision making and knowledge in efforts that promote welfare through the proper application of statistical principles and best practices, where access to such resources is limited.</p>
+        <h3>
+          We work to improve decision making and knowledge in efforts that
+          promote welfare through the proper application of statistical
+          principles and best practices, where access to such resources is
+          limited.
+        </h3>
       </React.Fragment>
     );
   }

545b0eb752ed6a7bb8d5c1bd53b40e2b0e3fb136
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-08-26 11:00:42 -0400

    Add use notes for callback

--- client/src/components/FormContact/FormContact.stories.js
+++ client/src/components/FormContact/FormContact.stories.js
@@ -3,16 +3,22 @@ import { storiesOf } from "@storybook/react";
 // import { action } from "@storybook/addon-actions";
 import FormContact from "./FormContact";
 
+//* Use notes:
+//* After you copy/paste <FormContact... into a component,
+//* change formCallback to formCallback={this.handleSend},
+//* create a method called handleSend in your component
+//* to post form data via your API.
+//* FormContact provides name, email, organization, subject,
+//* and message on submit, which you need to pass to handleSend
+//* handleSend(name, email, organization, subject, message)
+
 storiesOf("FormContact", module)
 	.add("FormContact", () => {
-		return (
-			// formCallback={this.handleSend}
-			<FormContact formCallback />
-		);
+		return <FormContact formCallback />;
 	})
 	.add("FormContactHelp", () => {
-		return <FormContact help formCallback={this.handleSend} />;
+		return <FormContact help formCallback />;
 	})
 	.add("FormContactVolunteer", () => {
-		return <FormContact volunteer formCallback={this.handleSend} />;
+		return <FormContact volunteer formCallback />;
 	});

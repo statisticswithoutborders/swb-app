db269c49ac188e0da1b07edbca76952059293d1f
butterfly1of4 <jhouck29@gmail.com>
2020-08-26 15:53:18 -0400

    fixed hr

--- client/src/components/BlogCard/BlogCard.js
+++ client/src/components/BlogCard/BlogCard.js
@@ -15,7 +15,7 @@ const BlogCard = (props) => {
     <div className={classList}>
       <div className="BlogCard-title">{props.title}</div>
       <div className="BlogCard-date">{props.date}</div>
-      <hr />
+      <BlogCard-hr />
       <div className="BlogCard-label">
         {props.label}
       </div>

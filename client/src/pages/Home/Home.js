1323bd7ceeaa762fb1e5ac3f21463010bf0acf7b
Jeremiah <jscott4720@gmail.com>
2020-08-26 19:07:18 -0400

    added Form component no functionality

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -10,6 +10,7 @@ import Testimonial from "../../components/Testimonial/Testimonial";
 import Footer from "../../components/Footer/Footer";
 import WhoWeAreCard from "../../components/WhoWeAreCard/WhoWeAreCard";
 import BlogCard from "../../components/BlogCard/BlogCard"
+import FormContact from "../../components/FormContact/FormContact"
 
 class Home extends Component {
   constructor() {
@@ -170,8 +171,12 @@ class Home extends Component {
         <hr className="homeHR" />
 
         <h2>Testimonials</h2>
-        <Testimonial />
-        <Testimonial />
+       <div className = 'testimonials'> <div><Testimonial description ={'Being an SWB volunteer is probably one of the best things I did. The experience helped me change my career track. Volunteering for SWB gave me experience that helped me move into the area of global health. I had the opportunity to learn from an expert(Gary Shapiro) about conducting surveys in a resource-poor country. We produced 6 publications and presentations and I wrote a chapter on Developing Leadership Skills through Volunteering for the book "Leadership and Women in statistics."'} name="Eugene" 
+      imgSrc="var(--home-india)" imgPostion="right"/></div>
+      <div> <Testimonial description ="Statistics Without Borders is an incredible way to give back to others. To be able to leverage the knowledge you've gained is an immensely rewarding experience. That being said, it is a commitment! We're using a huge network of volunteers to work with organizations around the world. It's important to be responsive and to communicate well with others, because we all rely on each other. If you're eager to get your hands into some volunteer work, SWB is a great place to look!" name="Ester" 
+      imgSrc="var(--home-yoel)" imgPostion="left"/>
+      </div>
+      </div>
         <hr className="homeHR" />
 
         <h2>SWB Blog</h2>
@@ -183,8 +188,7 @@ class Home extends Component {
         <hr className="homeHR" />
 
         <h2>Let's Work Together - Contact Us</h2>
-        <div>A form will go here</div>
-        <Button label="SEND" />
+        <div><FormContact formCallback={this.handleSend} /></div>
         <hr className="homeHR" />
         <Footer />
       </React.Fragment>

97cdfa7925772635211e35c3391fc87fac0518a5
mike_visser <mpviss@gmail.com>
2020-08-26 16:39:45 -0400

    adeed our volunteers and how it works sections

--- client/src/pages/VolunteersPage/VolunteersPage.js
+++ client/src/pages/VolunteersPage/VolunteersPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import "./VolunteersPage.css"
+import HowItWorks from '../../components/HowItWorks/HowItWorks'
 
 // React Class Page Component
 class VolunteersPage extends Component {
@@ -13,19 +14,32 @@ class VolunteersPage extends Component {
         
 
         return (
-            <div className={classList}>
+            <div className={this.state.classList}>
                 <div className="VolunteersPage-volunteerfor">
                     <h2 className="VolunteersPage-volunteerfor-title">Volunteer for SWB</h2>
                     <div className="VolunteersPage-volunteerfor-body">
                         <div className="VolunteersPage-volunteerfor-video"></div>
                         <div className="VolunteersPage-volunteerfor-content">
-                            <p>Volunteers are the engine that drive Statistics Without Borders: Without the time, energy & knowledge of our generous volunteers, we can’t provide our expertise to our in-need client groups.
-
+                            <p>Volunteers are the engine that drive Statistics Without Borders: Without the time, energy & knowledge of our generous volunteers, we can’t provide our expertise to our in-need client groups.<br/>
+                            <br/>
                             We have over 800 volunteers from 6 different continents. Our volunteers come from a wide variety of background across hundreds of countries.</p>
                         </div>
                     </div>
                 </div>
-
+                <div className="VolunteersPage-ourvolunteers">
+                    <h2 className="VolunteersPage-section-title">Our Volunteers</h2>
+                    <div className="VolunteersPage-volunteerfor-card-container">
+                        <div className="card-placeholder"></div>
+                        <div className="card-placeholder"></div>
+                        <div className="card-placeholder"></div>
+                        <div className="card-placeholder"></div>
+                    </div>
+                </div>
+                <div className="VolunteersPage-howitworks">
+                    <div className="VolunteersPage-hiw-container">
+                    <HowItWorks />
+                    </div>
+                </div>
             </div>
         )
     }

62799516866d07c9d1cedea4648fd98bc456d778
Jeremiah <jscott4720@gmail.com>
2020-08-25 20:08:38 -0400

    added 'Our Mission' with minimal styling

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import "./Home.css";
-import homeBanner from "../../assets/images/home_banner.png";
 import NavBar from "../../components/NavBar/NavBar";
 import Button from "../../components/Button/Button";
+import Globe from "../../assets/images/home_globe.png";
+import Analysis from "../../assets/images/home_analysis.png";
 
 class Home extends Component {
   constructor() {
@@ -15,7 +16,7 @@ class Home extends Component {
       <React.Fragment>
         <NavBar />
         <div className="banner">
-          <div className= 'words'>
+          <div className="words">
             <h1>We Use Data For Good</h1>
             <p className="statement">
               Statistics Without Borders is a non-profit volunteer outreach
@@ -26,6 +27,39 @@ class Home extends Component {
             <Button large label="LEARN MORE" />
           </div>
         </div>
+        <div className="mission">
+          <img src={Globe} />
+          <h2>Our Mission</h2>
+          <img src={Analysis} />
+        </div>
+        <h3>
+          We provide pro bono services in statistics and data science, to
+          improve decision making and knowledge in efforts that promote welfare
+          through the proper application of statistical principles and best
+          practices, where access to such resources is limited
+        </h3>
+        <ul>
+          <li>
+            Advocate objective, impartial, and sound decision making using best
+            statistical practices
+          </li>
+
+          <li>
+            Provide analytical services through client projects for public
+            benefit that support goals that are nonreligious, non-partisan, and
+            non-personal, with a focus on developing countries
+          </li>
+          <li>
+            Assist organizations outside of the for-profit sector, with
+            priorities given to organizations with limited access to statistical
+            resources
+          </li>
+          <li>
+            Strengthen its clients and their communities by helping them build
+            their capacities in statistics, data science and analytics, and
+            promotes best practices in these areas
+          </li>
+        </ul>
       </React.Fragment>
     );
   }

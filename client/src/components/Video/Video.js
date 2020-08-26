37f447c6b4a720eb3732f28cbabc472ca82b1359
Alex_Kelly <afkelly14@Gmail.com>
2020-08-25 21:06:41 -0400

    added project summary pictures, working on layout

---
+++ client/src/components/Video/Video.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./Video.css";
+import ReactPlayer from "react-player"
+
+
+function Video(props) {
+  let classList=""
+    if (props.large) {
+      classList+= ` Video-large`
+    }
+    if (props.small) {
+      classList+= ` Video-small`
+    }
+  return (
+    <div>
+      <ReactPlayer width= {props.width} height={props.height} className={classList} url={props.url} />
+    </div>
+  )
+}
+export default Video

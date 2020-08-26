040340eb12382953ee94cc99254cefef06b46992
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-08-26 10:42:32 -0400

    Make FormContact work with methods to capture key strokes and submit form data

--- client/src/components/FormContact/FormContact.js
+++ client/src/components/FormContact/FormContact.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 import "./FormContact.css";
 import Button from "../Button/Button";
-// import Input from "../Input/Input";
+import Input from "../Input/Input";
 
 // Controlled Component
 class FormContact extends Component {
 	constructor() {
 		super();
 		this.state = {
-			classlist: "FormContact",
 			name: "",
 			email: "",
 			organization: "",
@@ -35,45 +34,52 @@ class FormContact extends Component {
 	} //handleSubmit()
 
 	render() {
+		let classlist = "FormContact";
 		return (
-			<form className={this.state.classlist} onSubmit={this.handleSubmit}>
-				<label>
-					<input
-						name="name"
-						type="text"
-						placeholder=" NAME"
-						value={this.state.name}
-						onChange={this.handleChange}
-					/>
-				</label>
-				<label>
-					<input
-						name="email"
-						type="text"
-						placeholder=" EMAIL"
-						value={this.state.email}
-						onChange={this.handleChange}
-					/>
-				</label>
-				<label>
-					<input
-						name="organization"
-						type="text"
-						placeholder=" ORGANIZATION"
-						value={this.state.organization}
-						onChange={this.handleChange}
-					/>
-				</label>
-				<label>
-					<input
-						name="subject"
-						type="text"
-						placeholder=" SUBJECT"
-						value={this.state.subject}
-						onChange={this.handleChange}
-					/>
-				</label>
-				<label>
+			<form className={classlist} onSubmit={this.handleSubmit}>
+				<div className="FormContact-div-inputs">
+					<div className="FormContact-div-name-email">
+						<label className="FormContact-label-name">
+							<input
+								name="name"
+								type="text"
+								placeholder=" NAME"
+								value={this.state.name}
+								onChange={this.handleChange}
+							/>
+						</label>
+						<label className="FormContact-label-email">
+							<input
+								name="email"
+								type="text"
+								placeholder=" EMAIL"
+								value={this.state.email}
+								onChange={this.handleChange}
+							/>
+						</label>
+					</div>
+					<div className="FormContact-div-org-subject">
+						<label className="FormContact-label-organization">
+							<input
+								name="organization"
+								type="text"
+								placeholder=" ORGANIZATION"
+								value={this.state.organization}
+								onChange={this.handleChange}
+							/>
+						</label>
+						<label className="FormContact-label-subject">
+							<input
+								name="subject"
+								type="text"
+								placeholder=" SUBJECT"
+								value={this.state.subject}
+								onChange={this.handleChange}
+							/>
+						</label>
+					</div>
+				</div>
+				<label className="FormContact-label-message">
 					<textarea
 						name="message"
 						placeholder=" MESSAGE"

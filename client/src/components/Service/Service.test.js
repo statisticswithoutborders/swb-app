c96f2959cf89cd86c96768df440088e0e970c5c5
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-08-27 10:48:18 -0400

    Update import component name to Service

--- client/src/components/Service/Service.test.js
+++ client/src/components/Service/Service.test.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { shallow, mount, configure } from "enzyme";
-import FundingFact from "./FundingFact.js";
 import Adapter from "enzyme-adapter-react-16";
 configure({ adapter: new Adapter() });
+import Service from "./Service.js";
 
-describe("FundingFact component", () => {
-  it("should render as expected", () => {
-    const component = shallow(<FundingFact />);
-    expect(component.exists()).toBe(true);
-  });
+describe("Service component", () => {
+	it("should render as expected", () => {
+		const component = shallow(<Service />);
+		expect(component.exists()).toBe(true);
+	});
 });

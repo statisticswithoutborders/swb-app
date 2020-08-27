6abec27d5e5c8066c36150f7716dfead2c4bd4af
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-08-27 18:41:45 -0400

    Update initial render test

--- client/src/components/FormContact/FormContact.test.js
+++ client/src/components/FormContact/FormContact.test.js
@@ -1,14 +1,12 @@
-import React from 'react'
-import { shallow, mount, configure } from 'enzyme'
-import FormContact from './FormContact.js'
-import Adapter from 'enzyme-adapter-react-16';
-configure({adapter: new Adapter()});
+import React from "react";
+import { shallow, mount, configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
+import FormContact from "./FormContact.js";
 
-
-describe('FormContact component', () => {
-
-  it('should render as expected', () => {
-    const component = shallow(<FormContact />)
-	 	expect(component.exists()).toBe(true);
-  })
-})
+describe("<FormContact />", () => {
+	it("should render", () => {
+		const component = shallow(<FormContact />);
+		expect(component.exists()).toBe(true);
+	});
+});

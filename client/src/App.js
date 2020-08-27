b63209f8ca6946f149e630ca3a28ed003e1b64e3
Jeremiah <jscott4720@gmail.com>
2020-08-27 14:10:10 -0400

    added routes to current project pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,36 @@ import { Route, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import { render } from "enzyme";
-import Home from './pages/Home/Home'
-import NavBar from './components/NavBar/NavBar'
-import ContactUs from './pages/ContactUsPage/ContactUsPage'
-import Footer from './components/Footer/Footer'
+import Home from "./pages/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
+import ContactUs from "./pages/ContactUsPage/ContactUsPage";
+import Footer from "./components/Footer/Footer";
+import Projects from "./pages/ProjectSummary/ProjectSummary";
+import ProjectAsante from "./components/ProjectPage/ProjectPageAsante";
+import ProjectChimp from "./components/ProjectPage/ProjectPageChimp";
+import ProjectEthiopia from "./components/ProjectPage/ProjectPageEthiopia";
+import ProjectHaiti from "./components/ProjectPage/ProjectPageHaiti";
+import ProjectSyria from "./components/ProjectPage/ProjectPageSyria";
+import ProjectUnicef from "./components/ProjectPage/ProjectPageUnicef";
 
 function App() {
-   
-      return(
-        <React.Fragment>
+  return (
+    <React.Fragment>
       <div>
-        <NavBar/>
-        <Route path = "/" component = {Home} exact = {true} />
-        <Route path = "/contactus" component = {ContactUs} exact = {true} />
+        <NavBar />
+        <Route path="/" component={Home} exact={true}/>
+        <Route path="/contactus" component={ContactUs} exact={true}/>
+        <Route path="/projects/" component={Projects} exact={true}/>
+        <Route path="/projects/asante" component={ProjectAsante}/>
+        <Route path="/projects/chimp" component={ProjectChimp}/>
+        <Route path="/projects/ethiopia" component={ProjectEthiopia} />
+        <Route path="/projects/haiti" component={ProjectHaiti} />
+        <Route path="/projects/syria" component={ProjectSyria} />
+        <Route path="/projects/unicef" component={ProjectUnicef} />
+        <Footer />
       </div>
-      <Footer/>
-      </React.Fragment>
-      )
-    
+    </React.Fragment>
+  );
 }
 
 export default App;

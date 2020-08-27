ce4165d3abef0621d9af9b3db1fe5d60d2b8a7fa
butterfly1of4 <jhouck29@gmail.com>
2020-08-27 14:30:33 -0400

    updating

---
+++ api/swb/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+class Project(models.Model):
+    project_title = models.CharField(max_length=200)
+    project_tag = models.CharField(max_length=100)
+    project_location = models.CharField(max_length=100)
+    photo_urlOne = models.TextField(default='urlOne')
+    photo_urlTwo = models.TextField(default='urlTwo')
+    photo_urlThree = models.TextField(default='urlThree')
+    project_description = models.TextField(default='description')
+    
+    def __str__(self):
+        return self.project_title
+    
+    
+class Service(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='services')
+    project_service = models.CharField(max_length=200)
+       
+        
+    def __str__(self):
+        return self.project_service

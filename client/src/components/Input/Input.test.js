f0b4fa8f498672bf06371d9376a51c74afa11fee
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-08-27 15:17:45 -0400

    Install sinon, update Input tests

--- client/src/components/Input/Input.test.js
+++ client/src/components/Input/Input.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { shallow, mount, configure } from "enzyme";
+import sinon from "sinon";
 import Adapter from "enzyme-adapter-react-16";
 configure({ adapter: new Adapter() });
 import Input from "./Input";
@@ -10,5 +11,18 @@ describe("<Input />", () => {
 		expect(wrapper.exists()).toBe(true);
 	});
 
-	it("should render <input> with name = 'name'");
+	it("should render <input>", () => {
+		const wrapper = shallow(<Input />);
+		expect(wrapper.find("input")).toHaveLength(1);
+	});
+
+	it("should render <input>", () => {
+		const wrapper = shallow(
+			<Input name="name" type="text" placeholder=" NAME" value="" />
+		);
+		expect(wrapper.find("input")).to.have.lengthOf(1);
+	});
 });
+
+// <input name="name" type="text" placeholder=" NAME" value=""></input>
+// const callback = sinon.spy();

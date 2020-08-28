991e5fc5e3ad81699d0b58e1a9a85be262349d1a
mike_visser <mpviss@gmail.com>
2020-08-28 10:05:53 -0400

    added tests for funding fact image and text

--- client/src/components/FundingFact/FundingFact.test.js
+++ client/src/components/FundingFact/FundingFact.test.js
@@ -5,15 +5,28 @@ import Adapter from "enzyme-adapter-react-16";
 configure({ adapter: new Adapter() });
 
 describe("FundingFact component", () => {
-  it("should render as expected", () => {
-    const component = shallow(<FundingFact />);
-    expect(component.exists()).toBe(true);
-  });
-});
+  let leftDescription = "All donations are used 100% for SWB projects and are never used for payments or reimbursements."
+  let testImage = "var(--about-donate)"
+  const wrapper = shallow(<FundingFact image={testImage} description={leftDescription} />);
 
-describe("FundingFact component", () => {
-  it("has an image", () => {
-    const component = shallow(<FundingFact />);
-    expect(component.exists()).toBe(true);
+  it("should render as expected", () => {
+    expect(wrapper.exists()).toBe(true);
   });
+
+  it("has an image div", () => {
+    expect(wrapper.find('.FundingFact-image')).toHaveLength(1);
+  });
+  
+  it("has a description", () => {
+    expect(wrapper.find('.FundingFact-p')).toHaveLength(1);
+  });
+
+  it("image loads from props", () => {
+    expect(wrapper.contains(<div className='FundingFact-image' style={{backgroundImage: testImage}} />)).toBe(true);
+  });
+
+  it("props text loads", () => {
+    expect(wrapper.find('.FundingFact-p').text()).toEqual(leftDescription)
+  });
+
 });

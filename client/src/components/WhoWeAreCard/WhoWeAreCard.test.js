abfe4fed8f9f9dbe3b6330ed1f42895178da121e
mike_visser <mpviss@gmail.com>
2020-08-28 14:11:04 -0400

    added tests

--- client/src/components/WhoWeAreCard/WhoWeAreCard.test.js
+++ client/src/components/WhoWeAreCard/WhoWeAreCard.test.js
@@ -5,10 +5,59 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({adapter: new Adapter()});
 
 
+
+
 describe('WhoWeAreCard component', () => {
 
+  const wrapper = shallow(<WhoWeAreCard link='/' />)
+
   it('should render as expected', () => {
-    const component = shallow(<WhoWeAreCard />)
-	 	expect(component.exists()).toBe(true);
+    
+	 	expect(wrapper.exists()).toBe(true);
   })
+
+})
+
+describe('Contains All Component Elements', () =>{
+  const wrapper = shallow(<WhoWeAreCard link='/' />)
+
+  it('h2 renders as expected', () => {
+    
+	 	expect(wrapper.find('.WhoWeAreCard-title')).toHaveLength(1);
+  })
+  
+  it('image renders as expected', () => {
+    
+	 	expect(wrapper.find('.WhoWeAreCard-image')).toHaveLength(1);
+  })
+  
+  it('Button renders as expected', () => {
+    
+	 	expect(wrapper.find('Button').exists()).toBe(true);
+  })
+
+})
+
+describe('Component Props Render Correctly', () =>{
+  let label = "Our Team"
+  let image = "var(--home-members)"
+  let title = "Members"
+
+  const wrapper = shallow(<WhoWeAreCard title={title} buttonLabel={label} image={image} link='/' />)
+
+  it('Title prop renders', () => {
+    
+	 	expect(wrapper.find('.WhoWeAreCard-title').text()).toEqual(title)
+  })
+  
+  it('image prop renders', () => {
+    let style = wrapper.get(0).style
+    expect(style).toHaveProperty('backgroundImage', image)
+  })
+  
+  it('buttonLabel prop renders', () => {
+    
+    expect(wrapper.find('Button.label').text()).toEqual(title)
+  })
+
 })

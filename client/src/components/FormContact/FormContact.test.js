94dbeb290eab4a986e5554713b59d4750c5f998f
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-08-28 14:11:53 -0400

    Add test to change state with keystrokes

--- client/src/components/FormContact/FormContact.test.js
+++ client/src/components/FormContact/FormContact.test.js
@@ -5,8 +5,69 @@ configure({ adapter: new Adapter() });
 import FormContact from "./FormContact.js";
 
 describe("<FormContact />", () => {
+	const wrapperS = shallow(<FormContact />);
+	const wrapperM = mount(<FormContact />);
+	// const testName = "Joe";
+	// beforeAll(() => {
+	// 	const inputName = wrapperM.find("input").first();
+	// 	inputName.simulate("change", { target: { name: testName } });
+	// });
+
 	it("should render", () => {
-		const component = shallow(<FormContact />);
-		expect(component.exists()).toBe(true);
+		expect(wrapperS.exists()).toBe(true);
 	});
+
+	it("should render four <input> elements", () => {
+		expect(wrapperM.find("input")).toHaveLength(4);
+	});
+
+	it("should render one <textarea> element", () => {
+		expect(wrapperM.find("textarea")).toHaveLength(1);
+	});
+
+	it("should render help div", () => {
+		const wrapperHelp = shallow(<FormContact help />);
+		expect(wrapperHelp.find(".FormContact-div-help-volunteer")).toHaveLength(1);
+	});
+
+	it("should render volunteer div", () => {
+		const wrapperVol = shallow(<FormContact volunteer />);
+		expect(wrapperVol.find(".FormContact-div-help-volunteer")).toHaveLength(1);
+	});
+
+	it("should update state property name to 'Joe'", () => {
+		let input = wrapperM.find("input").first();
+		input.simulate("change", { target: { value: "Joe", name: "name" } });
+		expect(wrapperM.state("name")).toEqual("Joe");
+	});
+	// it("should submit data on submit", () => {
+	// const testState = {
+	// 		name: "Joe",
+	// 		email: "joe@g.com",
+	// 		organization: "Work Company",
+	// 		subject: "Just saying hiya",
+	// 		message: "Hiya Luna!",
+	// };
+
+	// const testCallback = (name, email, organization, subject, message) => {
+	// 	return "Joe, joe@g.com, Work Company, Just saying hiya, Hiya Luna!"
+	// };
+	// const expectedData =
+	// 	"Joe, joe@g.com, Work Company, Just saying hiya, Hiya Luna!";
+
+	// const myMock = jest.fn();
+	// const myMockData = () => {
+	//   let myData = myMock
+	// 	.mockReturnValueOnce("Joe")
+	// 	.mockReturnValueOnce("joe@g.com")
+	// 	.mockReturnValueOnce("Work Company")
+	// 	.mockReturnValueOnce("Just saying hiya")
+	//   .mockReturnValueOnce("Hiya Luna!");
+	//   return myData;
+	// };
+	// const wrapper = mount(<FormContact formCallback={myMockData} />);
+	// wrapper.find("[type='submit']").simulate("submit");
+	// expect(find(.))
+	// expect(testSubmit.toHaveBeenCalledWith(expectedData));
+	// });
 });

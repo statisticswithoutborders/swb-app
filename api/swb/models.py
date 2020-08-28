c5825a159b875144894b50068e9162f66a42234e
AnthonyMaddox <anthonyjmaddox@gmail.com>
2020-08-28 13:38:49 -0400

    added unique to service model

--- api/swb/models.py
+++ api/swb/models.py
@@ -2,25 +2,23 @@ from django.db import models
 
 # Create your models here.
 
+
 class Service(models.Model):
-    project_service = models.CharField(max_length=200)
-    
+    service_name = models.CharField(max_length=100, unique=True, default=" ")
+
     def __str__(self):
-        return self.project_service
+        return self.service_name
+
 
 class Project(models.Model):
     project_title = models.CharField(max_length=200)
-    project_tag = models.CharField(max_length=100)
-    project_location = models.CharField(max_length=100)
+    project_tag = models.CharField(max_length=100, default=" ")
+    project_location = models.CharField(max_length=100, default=" ")
     photo_urlOne = models.TextField(default='urlOne')
     photo_urlTwo = models.TextField(default='urlTwo')
     photo_urlThree = models.TextField(default='urlThree')
     project_description = models.TextField(default='description')
     project_services = models.ManyToManyField(Service)
-    
+
     def __str__(self):
         return self.project_title
-    
-       
-        
-  

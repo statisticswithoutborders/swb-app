a60ede918fb8dc5c8fdbeebdc60ff371886ba262
AnthonyMaddox <anthonyjmaddox@gmail.com>
2020-08-31 11:54:51 -0400

    succesfully seeded database with memember model

--- api/swb/admin.py
+++ api/swb/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Project
 from .models import Service
+from .models import Member
 
 # Register your models here.
 
 admin.site.register(Project)
 admin.site.register(Service)
+admin.site.register(Member)

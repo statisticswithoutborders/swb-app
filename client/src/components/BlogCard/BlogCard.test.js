c54dfc53f1ef31cde45af143408821ca5afc802a
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-08-31 14:56:44 -0400

    Test for elements having text truthy

--- client/src/components/BlogCard/BlogCard.test.js
+++ client/src/components/BlogCard/BlogCard.test.js
@@ -6,7 +6,27 @@ import BlogCard from "./BlogCard.js";
 
 describe("<BlogCard />", () => {
 	it("should render", () => {
-		const component = shallow(<BlogCard />);
-		expect(component.exists()).toBe(true);
+		const wrapper = shallow(<BlogCard />);
+		expect(wrapper.exists()).toBe(true);
+	});
+
+	it("should render a 'title' with text", () => {
+		const wrapper = shallow(<BlogCard title="Title: Blog Post 1" />);
+		expect(wrapper.find(".BlogCard-title").text()).toBeTruthy();
+	});
+
+	it("should render 'date' with text", () => {
+		const wrapper = shallow(<BlogCard date="Date: May 20, 2020" />);
+		expect(wrapper.find(".BlogCard-date").text()).toBeTruthy();
+	});
+
+	it("should render an <hr>", () => {
+		const wrapper = shallow(<BlogCard />);
+		expect(wrapper.find("hr")).toBeTruthy();
+	});
+
+	it("should render 'label' with text", () => {
+		const wrapper = shallow(<BlogCard label="Officia quis cupidatat." />);
+		expect(wrapper.find(".BlogCard-label").text()).toBeTruthy();
 	});
 });

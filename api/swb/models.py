238072b5f2693c8e9af599175257f67320d9873d
AnthonyMaddox <anthonyjmaddox@gmail.com>
2020-08-31 14:14:57 -0400

    successfully migrated form models with no seed data

--- api/swb/models.py
+++ api/swb/models.py
@@ -23,6 +23,7 @@ class Project(models.Model):
     def __str__(self):
         return self.project_title
 
+
 class Member(models.Model):
     member_name = models.CharField(max_length=100, unique=True, default=" ")
     member_picture_url = models.TextField(default=" ")
@@ -34,32 +35,36 @@ class Member(models.Model):
 
 
 class Contact(models.Model):
-    contact_name = models.CharField(max_length=100, default=" ")
-    contact_email: models.EmailField(max_length=200, default=" ")
-    contact_organization: models.CharField(max_length=100, default=" ")
-    contact_subject: models.CharField(max_length=100, default=" ")
-    contact_message: models.TextField(default=" ")
+    contact_name = models.CharField(max_length=100, default="NAME")
+    contact_email: models.EmailField(max_length=200, default="EMAIL")
+    contact_organization: models.CharField(
+        max_length=100, default="ORGANIZATION")
+    contact_subject: models.CharField(max_length=100, default="SUBJECT")
+    contact_message: models.TextField(default="MESSAGE")
 
     def __str__(self):
         return self.contact_name
 
 
 class Volunteer(models.Model):
-    volunteer_name = models.CharField(max_length=100, default=" ")
-    volunteer_email: models.EmailField(max_length=200, default=" ")
-    volunteer_organization: models.CharField(max_length=100, default=" ")
+    volunteer_name = models.CharField(max_length=100, default="NAME")
+    volunteer_email: models.EmailField(max_length=200, default="EMAIL")
+    volunteer_organization: models.CharField(
+        max_length=100, default="ORGANIZATION")
     volunteer_subject: models.CharField(max_length=100, default=" ")
-    volunteer_message: models.TextField(default=" ")
+    volunteer_message: models.TextField(default="MESSAGE")
 
     def __str__(self):
         return self.volunteer_name
 
+
 class NewProject(models.Model):
-    new_project_name = models.CharField(max_length=100, default=" ")
-    new_project_email: models.EmailField(max_length=200, default=" ")
-    new_project_organization: models.CharField(max_length=100, default=" ")
-    new_project_subject: models.CharField(max_length=100, default=" ")
-    new_project_message: models.TextField(default=" ")
+    new_project_name = models.CharField(max_length=100, default="NAME")
+    new_project_email: models.EmailField(max_length=200, default="EMAIL")
+    new_project_organization: models.CharField(
+        max_length=100, default="ORGANIZATION")
+    new_project_subject: models.CharField(max_length=100, default="SUBJECT")
+    new_project_message: models.TextField(default="MESSAGE")
 
     def __str__(self):
         return self.new_project_name

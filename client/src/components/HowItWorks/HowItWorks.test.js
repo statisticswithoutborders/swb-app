5bd928bdfb4175407dd0388a20db9e65c5a55b8b
mike_visser <mpviss@gmail.com>
2020-08-31 09:19:48 -0400

    added tests to check for images

--- client/src/components/HowItWorks/HowItWorks.test.js
+++ client/src/components/HowItWorks/HowItWorks.test.js
@@ -5,10 +5,23 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({adapter: new Adapter()});
 
 
-describe('HowItWorks component', () => {
+describe('HowItWorks component renders', () => {
 
   it('should render as expected', () => {
     const wrapper = shallow(<HowItWorks />)
 	 	expect(wrapper.exists()).toBe(true);
+  })
+})
+
+describe('Component Images Render', () => {
+
+  it('Step icons render', () => {
+    const wrapper = shallow(<HowItWorks />)
+	 	expect(wrapper.find('.HowItWorks-step-image')).toHaveLength(4);
+  })
+
+  it('Path image renders', () => {
+    const wrapper = shallow(<HowItWorks />)
+	 	expect(wrapper.find('.HowItWorks-path-image')).toHaveLength(1);
   })
 })

8610379a5997fb6f8b38bbe92f6e4766a1d1c0ab
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-08-31 20:15:17 -0400

    Install storybook-react-router, npm i

--- client/src/pages/ContactUsPage/ContactUsPage.test.js
+++ client/src/pages/ContactUsPage/ContactUsPage.test.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import { shallow, mount, configure } from 'enzyme'
-import ContactUsPage from './ContactUsPage.js'
-import Adapter from 'enzyme-adapter-react-16';
-configure({adapter: new Adapter()});
+import React from "react";
+import { shallow, mount, configure } from "enzyme";
+import ContactUsPage from "./ContactUsPage.js";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
 
+describe("ContactUsPage component", () => {
+	it("should render as expected", () => {
+		const component = shallow(<ContactUsPage />);
+		expect(component.exists()).toBe(true);
+	});
 
-describe('ContactUsPage component', () => {
-
-  it('should render as expected', () => {
-    const component = shallow(<ContactUsPage />)
-	 	expect(component.exists()).toBe(true);
-  })
-})
+	it("should render <form> element", () => {
+		const wrapper = mount(<ContactUsPage />);
+		expect(wrapper.find("form")).toHaveLength(1);
+	});
+});

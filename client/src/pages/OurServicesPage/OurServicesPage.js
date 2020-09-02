71080eacad2a23d305d435c82c38b3abfc79286c
AnthonyMaddox <anthonyjmaddox@gmail.com>
2020-09-02 12:49:24 -0400

    added dclass to main div and css

--- client/src/pages/OurServicesPage/OurServicesPage.js
+++ client/src/pages/OurServicesPage/OurServicesPage.js
@@ -50,7 +50,7 @@ class OurServicesPage extends Component {
 
 	render() {
 		return (
-			<div>
+			<div className="ourServicePageDiv">
 				<h1 class="title-services">What We Do</h1>
 				<p class="intro-services">
 					We work to improve decision making and knowledge in efforts that

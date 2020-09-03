f28ee5256056f595519d42b0aaa5fdb06e7036e6
Noah Clark <noahbc08@gmail.com>
2020-09-03 15:52:32 -0400

    added scroll feature

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,12 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
-
+import ScrollToTop from './ScrollToTop';
 ReactDOM.render(
   <Router>
+    <ScrollToTop>
     <App />
+    </ScrollToTop>
   </Router>,
   document.getElementById("root")
 );
